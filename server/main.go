9ad6bf5400a1261e9fce0771f87c5ab3b9167cf8
Pavlom <migkiros.p@gmail.com>
2024-08-07 16:38:17 +0300

    rpc calls

--- server/main.go
+++ server/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
 	"myDb/server/cfg"
+	"myDb/server/rpc"
 	"myDb/server/stateMachine"
+	"net"
 	"os"
 	"strconv"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -51,4 +60,61 @@ func main() {
 	}
 
 	fmt.Println(sc)
+	go startServer(int32(sc.Cfg.Me.Port))
+	startPing(sc)
+}
+
+func startPing(sc stateMachine.StateMachine) {
+	am := make(map[string]*string)
+	for {
+		time.Sleep(1 * time.Second)
+		for _, server := range sc.Cfg.Servers {
+			fmt.Println("Ping ", server.GetUrl())
+
+			a := rpc.AppendEntriesRequest{
+				Term: sc.Term,
+				Id:   sc.Cfg.Me.Id,
+			}
+
+			addr, ok := am[server.Id]
+			if !ok {
+				addr = flag.String("addr"+server.Id, server.GetUrl(), "the server to send")
+				am[server.Id] = addr
+			}
+
+			conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				fmt.Println("did not connect:", err)
+			}
+			defer conn.Close()
+			c := rpc.NewRaftServiceClient(conn)
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			r, err := c.SendAppendEntries(ctx, &a)
+			if err != nil {
+				fmt.Println("Could not contact server", server.Id, "with error:", err)
+			}
+			fmt.Println(r)
+		}
+	}
+}
+
+type server struct {
+	rpc.UnimplementedRaftServiceServer
+}
+
+func startServer(p int32) {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", p))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	rpc.RegisterRaftServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- server/stateMachine/stateMachine.go
+++ server/stateMachine/stateMachine.go
@@ -3,7 +3,7 @@ package stateMachine
 import "myDb/server/cfg"
 
 type StateMachine struct {
-	Term   int
+	Term   int32
 	Cfg    cfg.ServerConfig
 	Leader cfg.ServerIdentity
 	Role   cfg.ServerRole
